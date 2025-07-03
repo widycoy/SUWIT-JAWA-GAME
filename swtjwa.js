67d2bcc05f1c4a8cb583f6190452486d50161780
widycoy <widyaaditama@gmail.com>
2025-07-04 00:10:29 +0700

    Generated random numbers and captured comp choice

--- swtjwa.js
+++ swtjwa.js
@@ -1,2 +1,16 @@
 // Menangkap pilihan pemain
 var p = prompt("pilih : gajah, semut,orang");
+
+// Menangkap Pilihan Computer
+// Membangkitkan bilangan Random
+var comp = Math.random();
+
+if (comp < 0.34) {
+  comp = "gajah";
+} else if (comp >= 0.34 && comp < 0.67) {
+  comp = "orang";
+} else {
+  comp = "semut";
+}
+console.log(comp);
+
